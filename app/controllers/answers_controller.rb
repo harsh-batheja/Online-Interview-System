a1436ab10ac12e89a9bddf5bd6aa5d5441f0186a
Equinoxx9 <harshb012@gmail.com>
2019-08-19 18:24:47 +0530

    Added Answer Change on Reload

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,12 +9,13 @@ class AnswersController < ApplicationController
    end
 		@answer=Answer.new
 		@answer.question=@questions.first
+		@answer.test=@test
+		@answer.save
 	end
 
 	def create
-		answer=Answer.new(answer_params)
-		answer.test=@test
-		answer.save
+		answer=Answer.find_by(test_id: @test.id,question_id: answer_params[:question_id])
+		answer.update(answer_params)
 		redirect_to new_test_answer_path(@test)
 	end
 
